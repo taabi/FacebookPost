0d94cd2538a251425783c220e7d274eb7225c709
Tabish Ahmed <taabi@users.noreply.github.com>
2014-06-23 18:09:55 -0700

    Update README.m

--- README.m
+++ README.m
@@ -1,6 +1,6 @@
 FacebookPost
 ============
-Total Hours: 9
+Total Hours: 11
 
 Tasks:
 
@@ -10,10 +10,12 @@ Tasks:
 [completed] Add UIImageViews and choose the right mode: aspect fill, aspect fit, etc.
 [completed] Add a UITextField with a background image.
 [completed] Add UILabels with attributed text (instead of plain text), so that you can bold individual words.
-[Not complete] Setup CocoaPods for managing 3rd party libraries and use TTTAttributedLabel to style the link and make it clickable
+[complete] Setup CocoaPods for managing 3rd party libraries and use TTTAttributedLabel to style the link and make it clickable
 [completed] Optional: The like button should toggle the "liked" state.
 [completed] Optional: Move the textfield when the keyboard appears.
 [completed] Optional: The left and right navigation buttons should be real buttons. In other words, when you tap on them, they should have a highlight state. The buttons don't do anything when you tap on them.
 [completed] Optional: Tapping on the photo should show a view controller with a full-screen photo.
 [Not complete] Optional: Tapping on the tabs (requests, messages, etc) should show other view controllers.
 [Not complete] Optional: Implement the notifications page programatically without using Interface Builder. Don't use table views, just statically layout the views assuming no scrolling.
+
+[included]demo.gif 
